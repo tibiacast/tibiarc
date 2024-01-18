db70faa281f6d0c394053acf7d6de36d50c42216
John Högberg <git@hogberg.online>
2024-01-18 18:44:55 +0100

    tmv2: Simplify decompression

--- lib/formats/tmv2.c
+++ lib/formats/tmv2.c
@@ -26,92 +26,17 @@
 struct trc_recording_tmv2 {
     struct trc_recording Base;
 
-    bool Compressed;
-
     uint32_t FirstPacketTimestamp;
     uint32_t PacketNumber;
     uint32_t PacketCount;
 
-    struct trc_data_reader RawReader;
-    struct trc_data_reader DataReader;
-    uint8_t DataBuffer[128 << 10];
-
-    tinfl_decompressor Decompressor;
-    size_t DecompressedWritePos;
-    size_t DecompressedReadPos;
-    uint8_t DecompressionBuffer[128 << 10];
+    struct trc_data_reader Reader;
+    uint8_t *Uncompressed;
 };
 
-static bool tmv2_Decompress(struct trc_recording_tmv2 *recording,
-                            size_t desiredCount,
-                            void *buffer) {
-    if (recording->Compressed) {
-        while (recording->DecompressedWritePos <
-                       sizeof(recording->DecompressionBuffer) &&
-               datareader_Remaining(&recording->RawReader) > 0) {
-            size_t in_bytes, out_bytes;
-            tinfl_status status;
-
-            out_bytes = sizeof(recording->DecompressionBuffer) -
-                        recording->DecompressedWritePos;
-            in_bytes = datareader_Remaining(&recording->RawReader);
-
-            status = tinfl_decompress(
-                    &recording->Decompressor,
-                    (mz_uint8 *)datareader_RawData(&recording->RawReader),
-                    &in_bytes,
-                    (mz_uint8 *)&recording->DecompressionBuffer[0],
-                    (mz_uint8 *)&recording->DecompressionBuffer
-                            [recording->DecompressedWritePos],
-                    &out_bytes,
-                    TINFL_FLAG_HAS_MORE_INPUT);
-
-            ABORT_UNLESS(datareader_Remaining(&recording->RawReader) >=
-                         in_bytes);
-            datareader_Skip(&recording->RawReader, in_bytes);
-            recording->DecompressedWritePos += out_bytes;
-
-            if (status == TINFL_STATUS_DONE) {
-                break;
-            } else if (status != TINFL_STATUS_NEEDS_MORE_INPUT) {
-                return trc_ReportError("Failed to decompress data");
-            }
-        }
-
-        size_t availableBytes, copyBytes;
-
-        availableBytes = recording->DecompressedWritePos -
-                         recording->DecompressedReadPos;
-        copyBytes = MIN(desiredCount, availableBytes);
-
-        memcpy(buffer,
-               &recording->DecompressionBuffer[recording->DecompressedReadPos],
-               copyBytes);
-        recording->DecompressedReadPos += copyBytes;
-
-        if (availableBytes < desiredCount) {
-            if (datareader_Remaining(&recording->RawReader) == 0) {
-                return trc_ReportError("The input buffer has run dry");
-            } else {
-                recording->DecompressedWritePos = 0;
-                recording->DecompressedReadPos = 0;
-
-                return tmv2_Decompress(recording,
-                                       desiredCount - availableBytes,
-                                       &((char *)buffer)[copyBytes]);
-            }
-        }
-    } else {
-        if (!datareader_ReadRaw(&recording->RawReader, desiredCount, buffer)) {
-            return trc_ReportError("The input buffer has run dry");
-        }
-    }
-
-    return true;
-}
-
-static bool tmv2_DecompressPacket(struct trc_recording_tmv2 *recording) {
-    uint16_t packetLength;
+static bool tmv2_ProcessNextPacket(struct trc_recording_tmv2 *recording,
+                                   struct trc_game_state *gamestate) {
+    uint16_t outerLength, innerLength;
     uint32_t timestamp;
 
     if (recording->PacketNumber == recording->PacketCount) {
@@ -119,19 +44,12 @@ static bool tmv2_DecompressPacket(struct trc_recording_tmv2 *recording) {
         return true;
     }
 
-    if (!tmv2_Decompress(recording, 6, recording->DataBuffer)) {
-        return trc_ReportError("Could not decompress packet header");
+    if (!datareader_ReadU16(&recording->Reader, &outerLength)) {
+        return trc_ReportError("Could not read outer packet length");
     }
 
-    recording->DataReader.Length = 6;
-    recording->DataReader.Position = 0;
-
-    if (!datareader_ReadU16(&recording->DataReader, &packetLength)) {
-        return trc_ReportError("Could not decompress packet length");
-    }
-
-    if (!datareader_ReadU32(&recording->DataReader, &timestamp)) {
-        return trc_ReportError("Could not decompress packet timestamp");
+    if (!datareader_ReadU32(&recording->Reader, &timestamp)) {
+        return trc_ReportError("Could not read packet timestamp");
     }
 
     /* The timestamp refers to the current packet, not the next one, but I
@@ -139,64 +57,28 @@ static bool tmv2_DecompressPacket(struct trc_recording_tmv2 *recording) {
      * `recordings/sample.tmv2` in the wild. */
     recording->Base.NextPacketTimestamp =
             timestamp - recording->FirstPacketTimestamp;
-    recording->DataReader.Length = packetLength;
-    recording->DataReader.Position = 0;
     recording->PacketNumber++;
 
-    if (packetLength > 0) {
-        if (!tmv2_Decompress(recording, packetLength, recording->DataBuffer)) {
-            return trc_ReportError("Could not decompress packet data");
-        }
+    if (!datareader_ReadU16(&recording->Reader, &innerLength)) {
+        return trc_ReportError("Could not read inner packet length");
     }
 
-    return true;
-}
-
-static bool tmv2_HandleTibiaData(struct trc_recording_tmv2 *recording,
-                                 struct trc_game_state *gamestate) {
-    uint16_t subpacketLength;
-
-    if (!datareader_ReadU16(&recording->DataReader, &subpacketLength)) {
-        return trc_ReportError("Read failed on subpacketLength");
+    if (outerLength != (innerLength + 2)) {
+        return trc_ReportError("Corrupt packet length");
     }
 
-    if (datareader_Remaining(&recording->DataReader) != subpacketLength) {
-        return trc_ReportError("Bad sub-packet length");
+    struct trc_data_reader packetReader;
+    if (!datareader_Slice(&recording->Reader, innerLength, &packetReader)) {
+        return trc_ReportError("Could not read packet data");
     }
 
-    while (datareader_Remaining(&recording->DataReader) > 0) {
-        if (!parser_ParsePacket(&recording->DataReader, gamestate)) {
+    while (datareader_Remaining(&packetReader) > 0) {
+        if (!parser_ParsePacket(&packetReader, gamestate)) {
             return trc_ReportError("Failed to parse Tibia data packet");
         }
     }
 
     return true;
-}
-
-static bool tmv2_ProcessNextPacket(struct trc_recording_tmv2 *recording,
-                                   struct trc_game_state *gamestate) {
-    if (!tmv2_DecompressPacket(recording)) {
-        return trc_ReportError("Could not decompress packet");
-    }
-
-    if (!recording->Base.HasReachedEnd) {
-        return tmv2_HandleTibiaData(recording, gamestate);
-    }
-
-    return true;
-}
-
-static void tmv2_Rewind(struct trc_recording_tmv2 *recording,
-                        const struct trc_data_reader *reader) {
-    tinfl_init(&recording->Decompressor);
-
-    recording->Base.NextPacketTimestamp = recording->FirstPacketTimestamp;
-    recording->Base.HasReachedEnd = 0;
-
-    recording->RawReader = *reader;
-    recording->DecompressedWritePos = 0;
-    recording->DecompressedReadPos = 0;
-    recording->PacketNumber = 0;
 }
 
 static bool tmv2_QueryTibiaVersion(const struct trc_data_reader *file,
@@ -210,9 +92,7 @@ static bool tmv2_QueryTibiaVersion(const struct trc_data_reader *file,
     }
 
     uint8_t tibiaVersion[3];
-    if (!(datareader_ReadU8(&reader, &tibiaVersion[0]) &&
-          datareader_ReadU8(&reader, &tibiaVersion[1]) &&
-          datareader_ReadU8(&reader, &tibiaVersion[2]))) {
+    if (!datareader_ReadRaw(&reader, 3, tibiaVersion)) {
         return trc_ReportError("Failed to read Tibia version");
     }
 
@@ -227,13 +107,43 @@ static bool tmv2_QueryTibiaVersion(const struct trc_data_reader *file,
     return true;
 }
 
+static bool tmv2_DetermineLength(struct trc_recording_tmv2 *recording,
+                                 uint32_t packetCount) {
+    struct trc_data_reader reader = recording->Reader;
+    uint32_t minTimestamp = UINT32_MAX, maxTimestamp = 0;
+
+    while (--packetCount) {
+        uint16_t packetLength;
+        uint32_t timestamp;
+
+        if (!datareader_ReadU16(&reader, &packetLength) ||
+            !datareader_ReadU32(&reader, &timestamp) ||
+            !datareader_Skip(&reader, packetLength)) {
+            return trc_ReportError("Failed to read timestamp or skip payload");
+        }
+
+        minTimestamp = MIN(minTimestamp, timestamp);
+        maxTimestamp = MAX(maxTimestamp, timestamp);
+
+        if (minTimestamp > maxTimestamp) {
+            return trc_ReportError("Invalid timestamp in recording");
+        }
+    }
+
+    recording->Base.Runtime = maxTimestamp - minTimestamp;
+    recording->FirstPacketTimestamp = minTimestamp;
+
+    return true;
+}
+
 static bool tmv2_Open(struct trc_recording_tmv2 *recording,
                       const struct trc_data_reader *file,
                       struct trc_version *version) {
     struct trc_data_reader reader = *file;
 
-    uint32_t magic, options, packetCount, decompressedSize, firstTimestamp;
+    uint32_t magic, options, packetCount;
     uint16_t containerVersion;
+    size_t decompressedSize;
 
     if (!datareader_ReadU32(&reader, &magic)) {
         return trc_ReportError("Failed to skip magic");
@@ -246,6 +156,8 @@ static bool tmv2_Open(struct trc_recording_tmv2 *recording,
     if (!datareader_ReadU32(&reader, &options)) {
         return trc_ReportError("Failed to read options");
     }
+
+    recording->Compressed = options & 1;
 
     if (!datareader_ReadU16(&reader, &containerVersion)) {
         return trc_ReportError("Failed to read container version");
@@ -275,40 +187,41 @@ static bool tmv2_Open(struct trc_recording_tmv2 *recording,
         return trc_ReportError("Failed to read decompressed size");
     }
 
-    recording->Compressed = (options & 1);
+    if (recording->Compressed) {
+        /* Will be deallocated in tmv2_Free when set. */
+        recording->Uncompressed = checked_allocate(1, decompressedSize);
+
+        decompressedSize =
+                tinfl_decompress_mem_to_mem(recording->Uncompressed,
+                                            decompressedSize,
+                                            datareader_RawData(&reader),
+                                            datareader_Remaining(&reader),
+                                            0);
+
+        if (decompressedSize == TINFL_DECOMPRESS_MEM_TO_MEM_FAILED) {
+            return trc_ReportError("Could not decompress recording");
+        }
+
+        recording->Reader.Data = recording->Uncompressed;
+        recording->Reader.Length = decompressedSize;
+        recording->Reader.Position = 0;
+    } else {
+        recording->Reader = reader;
+    }
+
     recording->PacketCount = packetCount;
 
-    tmv2_Rewind(recording, &reader);
-
-    /* Manually determine the length of the file.
-     *
-     * This is really ugly but the only file I could find -- sample.tmv in
-     * the TibiaMovie repository -- appears to store the runtime instead of the
-     * last packet timestamp as documented. Luckily, manually determining the
-     * length works regardless of whether this number is correct or not. */
-    if (!tmv2_DecompressPacket(recording)) {
-        return trc_ReportError("Failed to determine first timestamp of file");
+    if (!tmv2_DetermineLength(recording, packetCount)) {
+        return trc_ReportError("Failed to determine length of recording");
     }
 
-    firstTimestamp = recording->Base.NextPacketTimestamp;
-
-    while (--packetCount) {
-        if (!tmv2_DecompressPacket(recording)) {
-            return trc_ReportError("Failed to determine runtime of file");
-        }
-    }
-
-    recording->Base.Runtime =
-            recording->Base.NextPacketTimestamp - firstTimestamp;
-    recording->FirstPacketTimestamp = firstTimestamp;
     recording->Base.Version = version;
-
-    tmv2_Rewind(recording, &reader);
 
     return true;
 }
 
 static void tmv2_Free(struct trc_recording_tmv2 *recording) {
+    checked_deallocate(recording->Uncompressed);
     checked_deallocate(recording);
 }
 
@@ -326,8 +239,6 @@ struct trc_recording *tmv2_Create() {
             (bool (*)(struct trc_recording *,
                       struct trc_game_state *))tmv2_ProcessNextPacket;
     recording->Base.Free = (void (*)(struct trc_recording *))tmv2_Free;
-
-    recording->DataReader.Data = &recording->DataBuffer[0];
 
     return (struct trc_recording *)recording;
 }
