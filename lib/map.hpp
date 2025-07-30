4595990cbaa8b949a8da305308cf07f9b4daa894
John Högberg <git@hogberg.online>
2025-07-30 12:42:12 +0200

    map: Don't store render height for out-of-bound coordinates

--- lib/map.hpp
+++ lib/map.hpp
@@ -71,19 +71,23 @@ struct Map {
     }
 
     uint8_t GetRenderHeight(int rX, int bY) const {
-        trc::Assert((rX / 32) >= 0 && (bY / 32) >= 0);
-        trc::Assert((rX / 32) + ((bY / 32) * TileBufferWidth) <=
-                    RenderHeightMapSize);
+        if (rX > 0 && bY > 0) {
+            trc::Assert((rX / 32) + ((bY / 32) * TileBufferWidth) <=
+                        RenderHeightMapSize);
 
-        return RenderHeightMap[(rX / 32) + ((bY / 32) * TileBufferWidth)];
+            return RenderHeightMap[(rX / 32) + ((bY / 32) * TileBufferWidth)];
+        }
+
+        return std::numeric_limits<uint8_t>::max();
     }
 
     void UpdateRenderHeight(int rX, int bY, uint8_t Z) {
-        trc::Assert((rX / 32) >= 0 && (bY / 32) >= 0);
-        trc::Assert((rX / 32) + ((bY / 32) * TileBufferWidth) <=
-                    RenderHeightMapSize);
+        if (rX > 0 && bY > 0) {
+            trc::Assert((rX / 32) + ((bY / 32) * TileBufferWidth) <=
+                        RenderHeightMapSize);
 
-        RenderHeightMap[(rX / 32) + ((bY / 32) * TileBufferWidth)] = Z;
+            RenderHeightMap[(rX / 32) + ((bY / 32) * TileBufferWidth)] = Z;
+        }
     }
 
     void Clear() {
